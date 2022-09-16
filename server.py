b3ac0ebd9f78e0a55cf69002a2d1a777d49b6d5b
Tweemo <timothyliew88@hotmail.com>
2022-09-16 14:17:26 +1200

    added new route to return a greeting

--- server.py
+++ server.py
@@ -8,12 +8,16 @@ app = Flask(__name__)
 
 # Members API Routes
 @app.route('/members')
-
 def members():
     '''Returns a list of members'''
     return {
       "members": ["Member 1", "Member 2", "Member 3"]
     }
+
+@app.route('/hello')
+def hello():
+    '''Returns a greeting'''
+    return "Hello World"
 
 if __name__ == '__main__':
     app.run(debug=True)
